82825cf1734b9aca3048992241d83ee523ff398b
Brian Groat <Barnum7@users.noreply.github.com>
2015-08-27 16:02:39 -0400

    completed Regular Expressions assignment

--- Ruby/regular_expressions.rb
+++ Ruby/regular_expressions.rb
@@ -1,6 +1,16 @@
-# Determine whether a string contains a SIN (Social Insurance Number).
-# A SIN is 9 digits and we are assuming that they must have dashes in them
+# # Determine whether a string contains a SIN (Social Insurance Number).
+# # A SIN is 9 digits and we are assuming that they must have dashes in them
+
+# =begin
+# Use regex to see if a string fits the pattern ###-###-###
+# If true puts that value and use it in the next function
+# =end
 def has_sin?(string)
+	  if string.match(/\b\d\d\d-\d\d\d-\d\d\d\b/) 
+	  	return true
+	  else 
+	  	return false
+	  end
 end
 
 puts "has_sin? returns true if it has what looks like a SIN"
@@ -13,7 +23,14 @@ puts has_sin?("please don't share this: 2342-604-142") == false
 puts has_sin?("please don't share this: 234-604-1421") == false
 
 # Return the Social Insurance Number from a string.
+=begin 
+If regex matches to a valid SIN. Return that valid SIN
+=end
 def grab_sin(string)
+	    valid_sin = string.match(/\b\d\d\d-\d\d\d-\d\d\d\b/)
+	    if valid_sin
+	    	return valid_sin[0]
+	    end
 end
 
 
@@ -24,8 +41,18 @@ puts "grab_sin returns nil if it doesn't have a SIN"
 puts grab_sin("please confirm your identity: XXX-XXX-142") == nil
 
 
-# Return all of the SINs from a string, not just one.
+# # Return all of the SINs from a string, not just one.
+=begin
+This is where it gets tricky. I can't just use match because that will only
+return the first result.
+PSYCH! Not tricky at all! I can just use .scan
+=end
+
 def grab_all_sins(string)
+	valid_sin = string.scan(/\b\d\d\d-\d\d\d-\d\d\d\b/)
+	if valid_sin
+		return valid_sin
+	end
 end
 
 puts "grab_all_sins returns all SINs if the string has any SINs"
@@ -35,9 +62,24 @@ puts "grab_all_sins returns an empty Array if it doesn't have any SINs"
 puts grab_all_sins("please confirm your identity: XXX-XXX-142") == []
 
 
-# Obfuscate all of the Social Insurance numbers in a string. Example: XXX-XX-4430.
+# Obfuscate all of the Social Insurance numbers in a string. Example: XXX-XXX-4430.
+=begin 
+Time to substitute all of the valid sins with astericks. Now our regex needs
+to store all matches AND substitute them with something else.
+=end 
 def hide_all_sins(string)
+	valid_sin = string.scan(/\b\d\d\d-\d\d\d-\d\d\d\b/)
+	if valid_sin.length > 0 
+		valid_sin.each do |sin|
+			sin.gsub!(/\b\d{3}(-)\d{3}/ , "XXX-XXX")
+		end
+		puts valid_sin
+		return valid_sin.join(", ")
+	else 
+		return string
+	end
 end
+		
 
 puts "hide_all_sins obfuscates any SINs in the string"
 puts hide_all_sins("234-601-142, 350-801-074, 013-601-876") == "XXX-XXX-142, XXX-XXX-074, XXX-XXX-876"
@@ -49,7 +91,16 @@ puts hide_all_sins(string) == string
 
 # Ensure all of the Social Insurance numbers use dashes for delimiters.
 # Example: 480.01.4430 and 480014430 would both be 480-01-4430.
+
+=begin
+I need to identify if a string is a possible SIN (in this case length = 9)
+Then I need to insure that there is a hyphen seperating digits 3,4 and 6,7
+=end
 def format_sins(string)
+	valid_sin = string.scan(/\b\d{9}\b/)
+	string.insert(3, ".")
+	string.insert(7, ".")
+	string.gsub!("." , "-")
 end
 
 puts "format_sins finds and reformat any SINs in the string"
