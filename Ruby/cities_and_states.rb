23823f73fe61bf971dc88d2a7cdaf4867b7c622e
Brian Groat <Barnum7@users.noreply.github.com>
2015-08-27 11:37:12 -0400

    Same code, just uncommented out the code segments that were commented out in initial commit

--- Ruby/cities_and_states.rb
+++ Ruby/cities_and_states.rb
@@ -6,8 +6,8 @@
   MI: 'Michigan'
 }
 # #Task Uno
-# @states[:AL] = 'Alaska'
-# @states[:KY] = 'Kentucky'
+@states[:AL] = 'Alaska'
+@states[:KY] = 'Kentucky'
 # # puts states
 
 # #Task Deux
@@ -17,21 +17,22 @@
 	NY: ['NYC','Ithika','Albany']	
 }
 
-# #Task Trois
-# # puts cities 
-# print "What state would you like to search?"
-# state_code = gets.chomp.upcase
-# def describe_state(state_code)
-# 	state_code = state_code.to_sym
-# 	full_name = @states[state_code]
-# 	cities_names = @cities[state_code]
-# 	full_cities_number = cities_names.count
-# 	cities_names_list = cities_names.join(".")
-# 	return  "#{state_code} is for #{full_name} and it has #{full_cities_number} \n
-# 	and those cities are #{cities_names_list}"
+#Task Trois
+
+
+print "What state would you like to search?"
+state_code = gets.chomp.upcase
+def describe_state(state_code)
+	state_code = state_code.to_sym
+	full_name = @states[state_code]
+	cities_names = @cities[state_code]
+	full_cities_number = cities_names.count
+	cities_names_list = cities_names.join(".")
+	return  "#{state_code} is for #{full_name} and it has #{full_cities_number}
+	and those cities are #{cities_names_list}"
 		
-# 	end
-# puts describe_state(state_code)
+	end
+puts describe_state(state_code)
 
 
 # #Task Quatre
@@ -45,21 +46,21 @@
 }
 
 
-#puts taxes
-#Task Cinco
-# print "What state are you in?"
-# state_code = gets.chomp.upcase
-# print "How much does the item cost?"
-# cost = gets.chomp.to_i
-# def calculate_tax(state_code,cost)
-# 	state_code = state_code.to_sym
-# 	tax_rate = @taxes[state_code]
-# 	tax_amount = cost * (0.01 * tax_rate)
-# 	return tax_amount.to_f.round(2)
-# 	puts "The tax on that item is #{tax_amount}"
-# end
+# Task Cinco
+print "What state are you in?"
+state_code = gets.chomp.upcase
+print "How much does the item cost?"
+cost = gets.chomp.to_i
+def calculate_tax(state_code,cost)
+	state_code = state_code.to_sym
+	tax_rate = @taxes[state_code]
+	tax_amount = cost * (0.01 * tax_rate)
+	return tax_amount.to_f.round(2)
+	puts "The tax on that item is #{tax_amount}"
+end
 
-# puts calculate_tax(state_code,cost)
+puts calculate_tax(state_code,cost)
+
 print "What city are you in?"
 city_name = gets.chomp.capitalize
 def find_state_for_city(city_name)
